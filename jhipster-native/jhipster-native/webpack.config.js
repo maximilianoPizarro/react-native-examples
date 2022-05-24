ad0ea90b3a4f119e1c76ca9a0ceeaf9029f4f194
Maximiliano Pizarro <mpizarro@datastar.com.ar>
2022-05-24 13:45:11 -0300

    expo upgrade 43.0.0

--- jhipster-native/jhipster-native/webpack.config.js
+++ jhipster-native/jhipster-native/webpack.config.js
@@ -5,7 +5,6 @@ module.exports = async function (env, argv) {
     {
       ...env,
       // Passing true will enable the default Workbox + Expo SW configuration.
-      offline: false,
     },
     argv,
   );
